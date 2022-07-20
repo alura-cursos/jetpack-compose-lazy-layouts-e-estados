5c94831fbcac93dc5ba393816f94578805c7848b
Alex Felipe <alexfelipevieira@gmail.com>
2022-07-20 09:59:01 -0300

    atualizando projeto

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -6,18 +6,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.alura.aluvery.R
 import br.com.alura.aluvery.model.Product
-import br.com.alura.aluvery.sampledata.sampleProducts
+import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.components.ProductsSection
-import java.math.BigDecimal
+import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    sections: Map<String, List<Product>>
+) {
     Column(
         Modifier
             .fillMaxSize()
@@ -25,31 +27,14 @@ fun HomeScreen() {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Spacer(Modifier)
-        ProductsSection("Promoções", sampleProducts)
-        ProductsSection(
-            "Doces", listOf(
-                Product(
-                    name = "Chocolate",
-                    price = BigDecimal("5.99"),
-                    image = R.drawable.chocolate
-                )
+        for (section in sections) {
+            val title = section.key
+            val products = section.value
+            ProductsSection(
+                title = title,
+                products = products
             )
-        )
-        ProductsSection(
-            "Bebidas", listOf(
-                Product(
-                    name = "Cerveja",
-                    price = BigDecimal("9.99"),
-                    image = R.drawable.beer
-                ),
-                Product(
-                    name = "Refrigerante",
-                    price = BigDecimal("3.99"),
-                    image = R.drawable.soda
-                ),
-
-                )
-        )
+        }
         Spacer(Modifier)
     }
 }
@@ -57,5 +42,9 @@ fun HomeScreen() {
 @Preview(showSystemUi = true)
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen()
+    AluveryTheme {
+        Surface {
+            HomeScreen(sampleSections)
+        }
+    }
 }
