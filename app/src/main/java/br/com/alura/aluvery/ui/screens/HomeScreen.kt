3b5b70a9bf23f5539a07214117cdef0b7436d135
Alex Felipe <alexfelipevieira@gmail.com>
2022-09-08 09:26:34 -0300

    3.2

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,10 +1,13 @@
 package br.com.alura.aluvery.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,20 +21,25 @@ import br.com.alura.aluvery.ui.theme.AluveryTheme
 fun HomeScreen(
         sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
+    Column {
+        OutlinedTextField(value = "alex", onValueChange = { newValue ->
+            Log.i("HomeScreen", "HomeScreen: TextField $newValue")
+        })
+        LazyColumn(
             Modifier
-                    .fillMaxSize(),
+                .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductsSection(
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(
                         title = title,
                         products = products
-                )
+                    )
+                }
             }
         }
     }
