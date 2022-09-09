7c661632edb0bc91afe3ecf7a49831cd385dac94
Alex Felipe <alexfelipevieira@gmail.com>
2022-09-09 10:04:18 -0300

    4.2

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,12 +1,18 @@
 package br.com.alura.aluvery.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.model.Product
@@ -31,7 +37,16 @@ fun HomeScreen(
                                 top = 16.dp,
                                 end = 16.dp)
                         .fillMaxWidth(),
-        shape = RoundedCornerShape(100))
+        shape = RoundedCornerShape(100),
+        leadingIcon = {
+            Icon(Icons.Default.Search, contentDescription = "ícone de lupa")
+        },
+        label = {
+            Text(text = "Produto")
+        },
+        placeholder = {
+            Text("O que você procura?")
+        })
         LazyColumn(
                 Modifier
                         .fillMaxSize(),
