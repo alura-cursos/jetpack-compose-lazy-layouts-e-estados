e8687ec110eb213a955217440541bc8fd721d347
Alex Felipe <alexfelipevieira@gmail.com>
2022-09-12 12:45:43 -0300

    6.3

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,19 +1,11 @@
 package br.com.alura.aluvery.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.model.Product
@@ -21,6 +13,7 @@ import br.com.alura.aluvery.sampledata.sampleProducts
 import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.components.CardProductItem
 import br.com.alura.aluvery.ui.components.ProductsSection
+import br.com.alura.aluvery.ui.components.SearchTextField
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 @Composable
@@ -29,28 +22,20 @@ fun HomeScreen(
         searchText: String = ""
 ) {
     Column {
-        var text by remember { mutableStateOf(searchText) }
-
-        OutlinedTextField(
-                value = text,
-                onValueChange = { newValue ->
-                    text = newValue
+        var text by remember {
+            mutableStateOf(searchText)
+        }
+        SearchTextField(
+                searchText = text,
+                onSearchChange = {
+                    text = it
                 },
                 Modifier
                         .padding(16.dp)
                         .fillMaxWidth(),
-        shape = RoundedCornerShape(100),
-        leadingIcon = {
-            Icon(Icons.Default.Search, contentDescription = "ícone de lupa")
-        },
-        label = {
-            Text(text = "Produto")
-        },
-        placeholder = {
-            Text("O que você procura?")
-        })
+        )
         val searchedProducts = remember(text) {
-            if(text.isNotBlank()) {
+            if (text.isNotBlank()) {
                 sampleProducts.filter { product ->
                     product.name.contains(
                             text,
@@ -69,7 +54,7 @@ fun HomeScreen(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 contentPadding = PaddingValues(bottom = 16.dp)
         ) {
-            if(text.isBlank()) {
+            if (text.isBlank()) {
                 for (section in sections) {
                     val title = section.key
                     val products = section.value
